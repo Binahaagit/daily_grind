b98dac9404e753e7cd34b8a3ac1b0c2f94d41ab7
Binahaagit <binahamary@gmail.com>
2026-06-06 23:47:24 +0530

    added top_k_freq_elements

--- 5.top_k_freq_elements.py
+++ 5.top_k_freq_elements.py
@@ -15,3 +15,12 @@ class Solution:
                 res.append(n)
                 if len(res) == k:
                     return res
+# Time Complexity: O(N) - We iterate through the nums array once to count the frequency of each element which takes O(N) time. Then through the frequency list, which takes O(N) time.
+# Space Complexity: O(N) - We use a hash map to store the frequency of each element which takes O(N) space. The frequency list also takes O(N) space.
+# - Use a hash map to count the frequency of each element in the nums array.
+# - Create a list of lists (frequency list) where the index represents the frequency and the value at that index is a list of elements that have that frequency.
+# - Iterate through the frequency list in reverse order and add elements to the result list until we have added k elements.
+
+# Alternative Approach: Using a Min Heap to keep track of the top k elements. This approach has a time complexity of O(N log k) due to the heap operations.
+
+# Another approach : Sorting the elements by frequency and then returning the top k elements. This approach has a time complexity of O(N log N) due to the sorting step.
